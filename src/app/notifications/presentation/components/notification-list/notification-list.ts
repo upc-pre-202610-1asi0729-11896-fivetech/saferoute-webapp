1ef7f268f8fbcaab95fe66194ea0a3d0388ec14d
Frase <ytcarniy@gmail.com>
2026-05-13 02:26:26 -0500

    chore: add bounded notifications

---
+++ src/app/notifications/presentation/components/notification-list/notification-list.ts
@@ -0,0 +1,21 @@
+import { Component, inject } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { TranslatePipe } from '@ngx-translate/core';
+import { NotificationStore } from '../../../application/notification-store';
+
+@Component({
+  selector: 'app-notification-list',
+  imports: [MatCardModule, MatListModule, MatIconModule, MatButtonModule, TranslatePipe],
+  templateUrl: './notification-list.html',
+  styleUrl: './notification-list.css'
+})
+export class NotificationList {
+  protected store = inject(NotificationStore);
+
+  markRead(id: number): void {
+    this.store.markAsRead(id);
+  }
+}
